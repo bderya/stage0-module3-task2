b4dfc1b0532cf18ee0d94c6408658b39cff2e130
Batuhan Derya <bderya@3-G-9.42heilbronn.de>
2022-09-24 13:41:25 +0200

    task3

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,12 +3,12 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable_1 = 2000;
+        int variable_one = 2000;
         int value = 1000;
-        int variable_2 = 4000;
-        int value_2 = 3000;
+        int variable_two = 4000;
+        int value_two = 3000;
         int variables = 5000;
         variables = 7000;
-        int value_3 = 90000;
+        int value_three = 90000;
     }
 }
